99c6d8ced6d695d2c9ba0d5f7cc526e769d2023b
yanagi <liuguzaoji@gmail.com>
2024-09-27 06:38:15 +0900

    READMEの修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->create();
+        User::factory(25)->create();
         Work::factory(100)->create();
         Rest::factory(300)->create();
     }
